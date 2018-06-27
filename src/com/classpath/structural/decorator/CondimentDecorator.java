e59b6387589040a39ff42086f41985ab487c9805
pradeep.kumar44@gmail.com <pradeep.kumar44@gmail.com>
2018-06-27 16:27:21 +0530

    committing the decorator design pattern

--- src/com/classpath/structural/decorator/CondimentDecorator.java
+++ src/com/classpath/structural/decorator/CondimentDecorator.java
@@ -1,6 +1,7 @@
 package com.classpath.structural.decorator;
 
 public abstract class CondimentDecorator extends Beverage {
+	
 	public Beverage beverage;
 
 	public abstract String getDescription();
--- src/com/classpath/structural/decorator/StarbuzzCoffee.java
+++ src/com/classpath/structural/decorator/StarbuzzCoffee.java
@@ -11,11 +11,11 @@ public class StarbuzzCoffee {
 		beverage2 = new Mocha(beverage2);
 		beverage2 = new Whip(beverage2);
 		System.out.println(beverage2.getDescription() + " INR " + beverage2.cost());
-
+		
 		Beverage beverage3 = new HouseBlend();
-		beverage3 = new Soy(beverage3);
-		beverage3 = new Mocha(beverage3);
-		beverage3 = new Whip(beverage3);
+		
+		beverage3 = new Soy(new Mocha(new Whip(beverage3)));
+		
 		System.out.println(beverage3.getDescription() + " INR" + beverage3.cost());
 	}
 }
